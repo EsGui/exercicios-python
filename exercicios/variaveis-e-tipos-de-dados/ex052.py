44363404c0a8282d64e7a8212a107bb357e40ec1
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2021-11-28 23:14:59 -0300

    Exercicios em andamento

--- exercicios/variaveis-e-tipos-de-dados/ex052.py
+++ exercicios/variaveis-e-tipos-de-dados/ex052.py
@@ -5,3 +5,24 @@ que leia quanto cada apostador investiu, o valor do prêmio, e imprima quanto ca
 ganharia do prêmio com base no valor investido.
 '''
 
+print('Divisão de prêmio!')
+print('-'*30)
+
+amigo1 = int(input('1º aposta: '))
+amigo2 = int(input('2º aposta: '))
+amigo3 = int(input('3º aposta: '))
+v_aposta = amigo1 + amigo2 + amigo3
+
+p_amigo1 = v_aposta * (amigo1 * 100 / v_aposta)
+
+print(int(p_amigo1))
+
+
+
+
+
+
+
+
+
+
