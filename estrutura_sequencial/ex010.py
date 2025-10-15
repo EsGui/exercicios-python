42001c69092f96a33d93e7c101b09ef8ed29515d
EsGui <determinationfullstack@gmail.com>
2025-10-14 21:58:52 -0300

    estrutura sequencial - exercicio 10 feito

---
+++ estrutura_sequencial/ex010.py
@@ -0,0 +1,11 @@
+"""
+
+Faça um programa que peça a temperatura em graus Celsius, e transforme em graus Fahrenheit
+
+"""
+
+c = float(input("Digite um valor em Celsius"))
+
+f = (c * 9/5) + 32
+
+print(f"Conversão = {f}")
