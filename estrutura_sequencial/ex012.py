ec79e11a20cbd2b26d581c7eaece374c379c0583
EsGui <determinationfullstack@gmail.com>
2025-10-14 22:08:38 -0300

    estrutura sequencial - exercicio 12 feito

---
+++ estrutura_sequencial/ex012.py
@@ -0,0 +1,13 @@
+"""
+
+Tendo como dados de entrada um arquivo em Gigabytes, construa um algoritmo que faça a conversão para Megabytes, usando a seguinte fórmula:
+
+Gigabytes * 1024
+
+"""
+
+arquivo = float(input("arquivo: "))
+
+mega_bytes = arquivo * 1024
+
+print(f"Conversão {megabytes}")
