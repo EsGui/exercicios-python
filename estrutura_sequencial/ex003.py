f8070db775105239da446076850c73eaa9fdb21c
EsGui <determinationfullstack@gmail.com>
2025-10-14 21:02:37 -0300

    estrutura sequencial - exercicio 3 feito

---
+++ estrutura_sequencial/ex003.py
@@ -0,0 +1,12 @@
+"""
+
+Faça um programa que peça dois número e imprima a soma
+
+"""
+
+n1 = int(input("Digite o primeiro valor: "))
+n2 = int(input("Digite o segundo valor: "))
+
+soma = n1 + n2
+
+print(f"A soma do valor {n1} e valor {n2} é igual a {soma}")
