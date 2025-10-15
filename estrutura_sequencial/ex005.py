58d52284ccba8d0c8b180dc5fb53cb3a82f60bcc
EsGui <determinationfullstack@gmail.com>
2025-10-14 21:21:31 -0300

    estrutura sequencial - exercicio 5 feito

---
+++ estrutura_sequencial/ex005.py
@@ -0,0 +1,11 @@
+"""
+
+Faça um programa que converta metros para centímetros
+
+"""
+
+metros = float(input("Digite um valor em metros: "))
+
+centimetros = metros * 100
+
+print(f"{metros} metros convertido para centimetros fica {centimetros} centimetros")
