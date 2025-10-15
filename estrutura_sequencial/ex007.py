68a751fb9bce703d09300b69bf00c3adbaba80c5
EsGui <determinationfullstack@gmail.com>
2025-10-14 21:47:31 -0300

    estrutura sequencial - exercicio 7 feito

---
+++ estrutura_sequencial/ex007.py
@@ -0,0 +1,11 @@
+"""
+
+Faça um programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário
+
+"""
+
+lado = float(input("Digite a base: "))
+
+dobro = (lado ** 2) * 2
+
+print(f"Dobro da área = {dobro}")
