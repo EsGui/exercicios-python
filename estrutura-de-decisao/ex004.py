4e9fd60f70d4de42bfca4bc0bb54018cc3f50036
EsGui <determinationfullstack@gmail.com>
2025-10-17 03:38:14 -0300

    Inciando estrutura sequencial - exercio 4 feito

---
+++ estrutura-de-decisao/ex004.py
@@ -0,0 +1,12 @@
+"""
+
+Faça um programa que verifique se uma letra digitada é vogal ou consoante
+
+"""
+
+letra = input("Digite uma letra: ")
+
+if letra in "aeiou":
+    print("Vogal")
+else:
+    print("Consoante")
