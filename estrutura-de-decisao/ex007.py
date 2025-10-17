535f1b6b7911bc23b4815ed93938315129c9a053
EsGui <determinationfullstack@gmail.com>
2025-10-17 04:12:59 -0300

    Inciando estrutura sequencial - exercio 7 feito

---
+++ estrutura-de-decisao/ex007.py
@@ -0,0 +1,28 @@
+"""
+
+Faça um programa que leia três números e mostre o maior e o menor deles:
+
+"""
+
+n1 = int(input("Digite o primeiro valor: "))
+n2 = int(input("Digite o segundo valor: "))
+n3 = int(input("Digite o terceiro valor: "))
+
+maior = 0
+menor = 0
+
+if n1 >= n2 and n1 >= n3:
+    maior = n1
+elif n2 >= n1 and n2 >= n3:
+    maior = n2
+else:
+    maior = n3
+
+if n1 <= n2 and n1 <= n3:
+    menor = n1
+elif n2 <= n1 and n2 <= n3:
+    menor = n2
+else:
+    menor = n3
+
+print(f"Maior número = {maior} / Menor número = {menor}")
