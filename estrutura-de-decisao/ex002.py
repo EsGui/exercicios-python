e084f5bf45cfb47a7a798545caf7400fe4aee3a1
EsGui <determinationfullstack@gmail.com>
2025-10-17 03:17:08 -0300

    Inciando estrutura sequencial - exercio 2 feito

---
+++ estrutura-de-decisao/ex002.py
@@ -0,0 +1,12 @@
+"""
+
+Faça um programa que peça um valor e mostra na tela se o valor é positivo ou negativo
+
+"""
+
+n1 = int(input("Digite um número: "))
+
+if n1 > 0:
+    print(f"{n1} = positivo")
+else:
+    print(f"{n1} = negativo")
