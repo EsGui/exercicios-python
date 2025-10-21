d4a109d274f004bb4f9885123278ec65a0ab4efa
EsGui <determinationfullstack@gmail.com>
2025-10-21 13:10:01 -0300

    listas - corrigindo exercicio 2

--- listas/ex002.py
+++ listas/ex002.py
@@ -4,10 +4,15 @@ Faça um programa que leia um vetor de 10 números reais e mostre-os na ordem in
 
 """
 
-vetor = [5, 7, 9, 3, 2, 4, 60, 85, 90, 8, 15]
-vetor_inverse = []
+vetor = []
+
+for c in range(1, 11):
+    numeros = int(input(f"Digite o {c}° valor: "))
+    vetor.append(numeros)
+
+vetor_inverso = []
 
 for i in range(len(vetor) - 1, -1, -1):
-  vetor_inverse.append(vetor[i])
+  vetor_inverso.append(vetor[i])
 
-print(vetor_inverse)
+print(vetor_inverso)
---
+++ listas/ex003.py
@@ -0,0 +1,5 @@
+"""
+
+Faça um programa que leia 4 notas, mostre as notas e a média na tela
+
+"""
